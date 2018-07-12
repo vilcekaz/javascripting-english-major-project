7c48aa8d86d7b82b0ae8c9e82e41257037750dac
KestutisKazlauskas <kstutisk4@gmail.com>
2018-07-12 19:02:42 +0300

    edit

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-alert("script.js has loaded");
+
+let userInput;
+userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
+if ( userInput === "burrito" ) {
+  $("#response").html("Brilliant choice!");
+} else {
+  $("#response").html("Don’t you want a burrito?");
+}
